37cb3b63109a5c4cb0dd0586b2cb86b19695b28b
ANTOPC\antos <anto@exordiumgames.com>
2026-07-11 01:21:02 +0200

    feature - added hotel search prompt parser interface

---
+++ HotelSearch.Application/Interfaces/IHotelSearchPromptParser.cs
@@ -0,0 +1,9 @@
+﻿using HotelSearch.Application.ValueObjects;
+
+namespace HotelSearch.Application.Interfaces
+{
+    public interface IHotelSearchPromptParser
+    {
+        Task<HotelSearchParameters> ParseSearchPromptAsync(string prompt);
+    }
+}
