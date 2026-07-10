cfd3df7162c37dfe0f58bd02992a727a0db6a1bc
ANTOPC\antos <anto@exordiumgames.com>
2026-07-11 00:43:05 +0200

    feature - added search dtos and hotel search service interface

---
+++ HotelSearch.Application/DTOs/HotelSearchResultDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelSearch.Application.DTOs
+{
+    public class HotelSearchResultDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public double DistanceInKilometers { get; set; }
+    }
+}
